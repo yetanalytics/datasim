cfd796c511b3dd9f786f793b6e8e4a24da4a5d71
Milt Reder <milt@yetanalytics.com>
2019-11-20 16:06:50 -0500

    better alignment for pattern gen

--- src/main/com/yetanalytics/datasim/xapi/profile.clj
+++ src/main/com/yetanalytics/datasim/xapi/profile.clj
@@ -18,9 +18,18 @@
               {:keys [id] :as thing} things]
           [id thing])))
 
+(defn align-likelihood
+  [actor-alignment
+   iri
+   & {:keys [base]
+      :or {base 1.0}}]
+  (max 0.01
+       (+ base
+          (get actor-alignment iri 0.0))))
 
 (defn pattern-gen*
   [profile-map
+   actor-alignment
    iri]
   (let [{:keys [id
                 type
@@ -33,49 +42,94 @@
                 zeroOrMore
                 ]
          {label :en}:prefLabel}
+        ;; We currently assume everything in one profile, this needs to change
         (get profile-map iri)]
     (case type
       "Pattern"
       (cond
         alternates
-        (gen/one-of (mapv (partial
-                           pattern-gen*
-                           profile-map)
-                          alternates))
+        (gen/bind
+         (gen/shuffle alternates)
+         (fn [alternates']
+           (gen/frequency (mapv
+                           (fn [alt-iri]
+                             [(align-likelihood
+                               actor-alignment
+                               alt-iri)
+                              (pattern-gen*
+                               profile-map
+                               actor-alignment
+                               alt-iri)])
+                           alternates'))))
+
+        ;; TODO: figure out more rigourous effect
         optional
-        (gen/one-of [(gen/return [])
-                     (pattern-gen*
-                      profile-map
-                      (:id optional))])
+        (gen/frequency [[1.0
+                         (gen/return [])]
+                        [(align-likelihood
+                          actor-alignment
+                          (:id optional))
+                         (pattern-gen*
+                          profile-map
+                          actor-alignment
+                          (:id optional))]])
+        ;; TODO: Figure out effect, if any
         oneOrMore
         (gen/vector (pattern-gen*
                      profile-map
+                     actor-alignment
                      (:id oneOrMore))
                     1 100)
         sequence
         (apply gen/tuple (mapv (partial
                                 pattern-gen*
-                                profile-map)
+                                profile-map
+                                actor-alignment)
                                sequence))
         zeroOrMore
-        (gen/vector (pattern-gen*
-                     profile-map
-                     (:id zeroOrMore))))
+        (gen/frequency
+         [[1.0 (gen/return [])]
+          [(align-likelihood
+            actor-alignment
+            (:id zeroOrMore))
+           (gen/vector (pattern-gen*
+                        profile-map
+                        actor-alignment
+                        (:id zeroOrMore))
+                       1 100)]]))
       "StatementTemplate"
       (gen/return iri))))
 
 (defn pattern-gen
   "Generator that, given a profile and a primary pattern iri, returns a valid
-  walk of the pattern."
-  [profile iri]
-  (let [profile-map (iri-map profile)
-        {:keys [primary
-                type]} (get profile-map iri)]
-    (assert (= type "Pattern") "IRI must be for a pattern")
-    (assert primary "Can only generate a primary pattern.")
-    (gen/fmap flatten
-              (pattern-gen* profile-map
-                            iri))))
+  walk of the pattern. If IRI is omitted, it will constuct a gen that picks
+  primary patterns based on alignment."
+  [profile
+   actor-alignment
+   & [iri]]
+  (let [profile-map (iri-map profile)]
+    (gen/bind
+     (if iri
+       (gen/return iri)
+       (gen/frequency
+        (mapv
+         (fn [id]
+           [(align-likelihood actor-alignment id)
+            (gen/return id)])
+         (keep
+          (fn [[id {:keys [type primary]}]]
+            (when (and (= type "Pattern") primary)
+              id))
+          profile-map))))
+     (fn [iri]
+       (let [{:keys [primary
+                     type]} (get profile-map iri)]
+         (assert (= type "Pattern") "IRI must be for a pattern")
+         (assert primary "Can only generate a primary pattern.")
+         (gen/fmap flatten
+                   (pattern-gen* profile-map
+                                 actor-alignment
+                                 iri)))))))
 
 
 
@@ -92,11 +146,19 @@
                                      id))
                                  (iri-map p)))
 
-  (gen/generate
+  (time (first (gen/generate
    (pattern-gen
-    p
-    "https://w3id.org/xapi/cmi5#toplevel")
-   4)
+    p ;; profile
+    {} #_{"https://w3id.org/xapi/cmi5#satisfied" -1.0} ;; alignment
+    ;; "https://w3id.org/xapi/cmi5#toplevel" ;; entry pattern
+    )
+   3 ;; gen 'size'
+   1234
+   )))
+
+
+
+
 
 
   )
