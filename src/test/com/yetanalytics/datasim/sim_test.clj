c7b7d10e0f3d43515fb85ff3a2ebd8e31060ebf6
Milt Reder <milt@yetanalytics.com>
2019-11-20 13:03:55 -0500

    improved sim tests

--- src/test/com/yetanalytics/datasim/sim_test.clj
+++ src/test/com/yetanalytics/datasim/sim_test.clj
@@ -4,8 +4,26 @@
             [com.yetanalytics.datasim.input :as input]
             [clojure.spec.alpha :as s]))
 
+(def valid-input
+  (input/from-location
+   :input :json "dev-resources/input/simple.json"))
+
 (deftest build-skeleton-test
-  (is (s/valid? :com.yetanalytics.datasim.sim/skeleton
-                (build-skeleton
-                 (input/from-location
-                  :input :json "dev-resources/input/simple.json")))))
+  (testing "given valid input, returns a valid skeleton"
+    (is (s/valid?
+         :com.yetanalytics.datasim.sim/skeleton
+         (build-skeleton
+          valid-input)))))
+
+(deftest disjoint-rng-test
+  (testing "Make sure RNGs aren't shared across threads."
+    (let [skeleton (build-skeleton valid-input)]
+      (are [actor-id] (let [prob-seq (get-in skeleton
+                                             [actor-id
+                                              :prob-seq])
+                            f1 (future (nth prob-seq 1000))
+                            f2 (future (nth prob-seq 1000))]
+                        (= @f1 @f2))
+        "mbox::mailto:alice@example.org"
+        "mbox::mailto:bob@example.org"
+        "mbox::mailto:fred@example.org"))))
