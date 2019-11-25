43db348eb548c3879c00cdfd0e01af22d6fbab01
Milt Reder <milt@yetanalytics.com>
2019-11-25 12:46:33 -0500

    profile pattern zipper for determ. walks

--- src/main/com/yetanalytics/datasim/xapi/profile.clj
+++ src/main/com/yetanalytics/datasim/xapi/profile.clj
@@ -1,168 +1,170 @@
 (ns com.yetanalytics.datasim.xapi.profile
   "Understanding elements of xAPI profiles"
-  (:require [clojure.test.check.generators :as gen]))
+  (:require [com.yetanalytics.datasim.random :as random]
+            [clojure.zip :as z])
+  (:import [java.util Random]))
 
-(defn all-iris
-  "Given a profile, return all IRIs"
-  [profile]
-  (for [[_ things] (select-keys profile [:concepts :patterns :templates])
-        {:keys [id] :as thing} things]
-    id))
-
-
-(defn iri-map
-  "Given a profile, return all IRIs mapped to what they reference"
-  [profile]
+(defn profiles->map
+  "Given any number of profiles, return all IRIs mapped to what they reference"
+  [& profiles]
+  (assert (seq profiles) "At least one profile is required.")
   (into {}
-        (for [[_ things] (select-keys profile [:concepts :patterns :templates])
+        (for [profile profiles
+              [_ things] (select-keys profile [:concepts :patterns :templates])
               {:keys [id] :as thing} things]
           [id thing])))
 
-(defn align-likelihood
-  [actor-alignment
-   iri
-   & {:keys [base]
-      :or {base 1.0}}]
-  (max 0.01
-       (+ base
-          (get actor-alignment iri 0.0))))
+(defn loc-iri-map
+  "Given a zipper loc, get the map of profile subobjects by IRI"
+  [loc]
+  (get (meta loc) ::iri-map))
 
-(defn pattern-gen*
-  [profile-map
-   actor-alignment
-   iri]
-  (let [{:keys [id
-                type
-                primary
+(defn loc-object
+  "Given a zipper loc, look up the object from the IRI"
+  [loc]
+  (get (loc-iri-map loc) (z/node loc)))
 
+(defn root-node?
+  [node-iri]
+  (= ::root node-iri))
+
+(defn pattern-zip
+  "Given one or more profiles, create a zipper that traverses the patterns and
+  templates. Root is a special keyword ::root with the primary patterns as
+  `alternates` children."
+  [& profiles]
+  (let [iri-map' (apply profiles->map profiles)
+        primary-pattern-iris
+        (keep
+         (fn [{id :id
+               node-type :type
+               primary :primary}]
+           (when (and (= node-type "Pattern")
+                      primary)
+             id))
+         (vals iri-map'))]
+    (assert (seq primary-pattern-iris)
+            "Profile input must contain primary patterns!")
+    ;; Add the virtual top level before proceeding
+    (let [iri-map (assoc iri-map'
+                         ::root
+                         {:id ::root
+                          :type "Pattern"
+                          :alternates (into [] primary-pattern-iris)})]
+      (->
+       (z/zipper
+        (fn branch? [node-iri]
+          (let [{node-type :type} (get iri-map node-iri)]
+            (= node-type "Pattern")))
+        ;; The default children function leaves all options open
+        ;; We can override :zip/children to make it decide things.
+        (fn children [node-iri]
+          (let [{:keys [sequence
+                        alternates
+                        optional
+                        oneOrMore
+                        zeroOrMore
+                        ]} (get iri-map node-iri)]
+            (or sequence
                 alternates
-                optional
-                oneOrMore
-                sequence
-                zeroOrMore
-                ]
-         {label :en}:prefLabel}
-        ;; We currently assume everything in one profile, this needs to change
-        (get profile-map iri)]
-    (case type
-      "Pattern"
-      (cond
-        alternates
-        (gen/bind
-         (gen/shuffle alternates)
-         (fn [alternates']
-           (gen/frequency (mapv
-                           (fn [alt-iri]
-                             [(align-likelihood
-                               actor-alignment
-                               alt-iri)
-                              (pattern-gen*
-                               profile-map
-                               actor-alignment
-                               alt-iri)])
-                           alternates'))))
+                (and optional
+                     [optional])
+                (and oneOrMore
+                     [oneOrMore])
+                (and zeroOrMore
+                     [zeroOrMore]))))
+        ;; Make node doesn't really mean much here, as we don't edit.
+        (fn make-node
+          [node-iri kid-iris]
+          node-iri)
+        ::root)
+       ;; We store the map in meta, which is always preserved
+       (vary-meta assoc ::iri-map iri-map)))))
 
-        ;; TODO: figure out more rigourous effect
-        optional
-        (gen/frequency [[1.0
-                         (gen/return [])]
-                        [(align-likelihood
-                          actor-alignment
-                          optional)
-                         (pattern-gen*
-                          profile-map
-                          actor-alignment
-                          optional)]])
-        ;; TODO: Figure out effect, if any
-        oneOrMore
-        (gen/vector (pattern-gen*
-                     profile-map
-                     actor-alignment
-                     oneOrMore)
-                    1 100)
-        sequence
-        (apply gen/tuple (mapv (partial
-                                pattern-gen*
-                                profile-map
-                                actor-alignment)
-                               sequence))
-        zeroOrMore
-        (gen/frequency
-         [[1.0 (gen/return [])]
-          [(align-likelihood
-            actor-alignment
-            zeroOrMore)
-           (gen/vector (pattern-gen*
-                        profile-map
-                        actor-alignment
-                        zeroOrMore)
-                       1 100)]]))
-      "StatementTemplate"
-      (gen/return iri))))
+(defn rand-pattern-zip
+  "Building on the comprehension from pattern-zip, return a zipper that uses a
+  deterministic pseudorandom walk for choosing children/paths."
+  [alignment
+   ^Random rng
+   profiles
+   & {:keys [repeat-max]
+      :or {repeat-max 5}}]
+  (let [pzip (apply pattern-zip profiles)
+        iri-map (::iri-map (meta pzip))]
+    (vary-meta pzip
+               assoc
+               :zip/children
+               (fn rand-children
+                 [node-iri]
+                 (let [{:keys [id
+                               type
+                               primary
 
-(defn pattern-gen
-  "Generator that, given a profile and a primary pattern iri, returns a valid
-  walk of the pattern. If IRI is omitted, it will constuct a gen that picks
-  primary patterns based on alignment."
-  [profile
-   actor-alignment
-   & [iri]]
-  (let [profile-map (iri-map profile)]
-    (gen/bind
-     (if iri
-       (gen/return iri)
-       (gen/frequency
-        (mapv
-         (fn [id]
-           [(align-likelihood actor-alignment id)
-            (gen/return id)])
-         (keep
-          (fn [[id {:keys [type primary]}]]
-            (when (and (= type "Pattern") primary)
-              id))
-          profile-map))))
-     (fn [iri]
-       (let [{:keys [primary
-                     type]} (get profile-map iri)]
-         (assert (= type "Pattern") "IRI must be for a pattern")
-         (assert primary "Can only generate a primary pattern.")
-         (gen/such-that
-          not-empty
-          (gen/fmap flatten
-                    (pattern-gen* profile-map
-                                  actor-alignment
-                                  iri))))))))
+                               alternates
+                               optional
+                               oneOrMore
+                               sequence
+                               zeroOrMore
+                               ]
+                        {label :en} :prefLabel
+                        :as profile-obj} (get iri-map node-iri)]
+                   (cond
+                     alternates
+                     [(random/choose rng alignment alternates)]
 
-
-
+                     optional
+                     (if-let [yeah (random/choose rng alignment [nil optional])]
+                       [optional]
+                       [])
+                     ;; TODO: Figure out effect, if any
+                     oneOrMore
+                     (repeat (inc (random/rand-int* rng repeat-max))
+                             oneOrMore)
+                     zeroOrMore
+                     (repeat (random/rand-int* rng repeat-max)
+                             zeroOrMore)
+                     sequence
+                     sequence
+                     ))))))
 
 
 
 
 (comment
+
+
   (require '[com.yetanalytics.datasim.input :as i])
 
   (def p
     (i/from-location
      :profile :json "dev-resources/profiles/cmi5/fixed.json"))
 
-  (com.yetanalytics.datasim.protocols/validate p)
+  (-> p
+      :templates
+      (->> (mapcat :rules))
+      clojure.pprint/pprint)
 
-  (clojure.pprint/pprint (keep (fn [[id {:keys [type primary]}]]
-                                   (when (and (= type "Pattern")
-                                              primary)
-                                     id))
-                                 (iri-map p)))
 
-  (time (gen/generate
-         (pattern-gen
-          p ;; profile
-          {} #_{"https://w3id.org/xapi/cmi5#satisfied" -1.0} ;; alignment
-          "https://w3id.org/xapi/cmi5#toplevel" ;; entry pattern
-          )
-         0 ;; gen 'size'
-         123
-         ))
+
+  ;; traverse randomly
+  (-> (rand-pattern-zip
+       {} (random/seed-rng 45)
+       [p])
+      (->> (iterate z/next)
+           (take-while (complement z/end?))
+           #_(keep z/node)
+           (filter #(= "StatementTemplate"
+                       (:type (loc-object %))))
+           (keep (juxt z/node
+                       z/path))
+
+           (into [])
+           )
+
+
+      )
+
+
 
 
 
