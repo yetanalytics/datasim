22fc7a5acf433b5e14724bccbcf78394927eeea3
Milt Reder <milt@yetanalytics.com>
2019-11-25 13:30:26 -0500

    stubbed out statement gen fn

---
+++ src/main/com/yetanalytics/datasim/xapi/statement.clj
@@ -0,0 +1,75 @@
+(ns com.yetanalytics.datasim.xapi.statement
+  "Generate Statements"
+  (:require [clojure.spec.alpha :as s]
+            [com.yetanalytics.datasim.xapi.profile :as profile]
+            [com.yetanalytics.datasim.input :as input]
+            com.yetanalytics.datasim.input.alignments
+            [com.yetanalytics.datasim.input.personae :as personae]
+            [com.yetanalytics.pan.objects.template :as template]
+            [xapi-schema.spec :as xs]))
+
+;; The duration, in milliseconds, of the returned statement
+;; This is so we can resume processing AFTER the statement timestamp + duration
+(s/def ::duration-ms
+  pos-int?)
+
+(s/def ::meta
+  (s/keys :req-un [::duration-ms]))
+
+(s/def ::alignment
+  :alignment-map/actor-alignment)
+
+(s/def ::sim-t pos-int?)
+
+(s/def ::seed int?)
+
+(s/def ::registration
+  ::xs/uuid)
+
+(s/def ::sub-registration
+  ::xs/uuid)
+
+(s/fdef generate-statement
+  :args
+  (s/cat
+   :args-map
+   (s/keys*
+    :req-un [;; input for the whole simulation
+             :com.yetanalytics.datasim/input
+             ;; flat map of profile iris to objects
+             ::profile/iri-map
+             ;; the actor for the statement
+             ;; (may have to stringify keys?)
+             ::xs/actor
+             ;; their alignment, a map of IRI to -1.0->1.0
+             ::alignment
+             ;; a statement template to generate from
+             ::template/template
+             ;; Simulation time, in ms since epoch
+             ::sim-t
+             ;; A seed to generate with. Note that if you're calling more seeded
+             ;; generators, you'll need to make a seed from this one for each.
+             ::seed
+             ;; A registration UUID string
+             ::registration]
+    :opt-un [::sub-registration]))
+  :ret (s/and ::xs/statement
+              (s/conformer meta)
+              ::meta))
+
+(defn generate-statement
+  [{{} :input
+    iri-map :iri-map
+    actor :actor
+    alignment :alignment
+    sim-t :sim-t
+    seed :seed
+    {template-iri :id} :template
+    registration :registration
+    ?sub-registration :sub-registration}]
+
+  (with-meta
+    ;; The generated statement
+    {}
+    ;; The duration in MS so we can continue the sim
+    {:duration-ms 1000}))
