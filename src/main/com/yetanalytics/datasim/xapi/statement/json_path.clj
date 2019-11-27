10766751c2b084a3746eaf9c077feae356ddc93a
William Hoyt <will@yetanalytics.com>
2019-11-27 14:09:01 -0500

    added notes about necessary improvements to handle-json-path-str and the fn will return anything after the vector close as :rest

--- src/main/com/yetanalytics/datasim/xapi/statement/json_path.clj
+++ src/main/com/yetanalytics/datasim/xapi/statement/json_path.clj
@@ -11,16 +11,36 @@
    - `path` everything up to the array
    - `nested` everything inbetween [' and ']"
   [path-str]
+  ;; FIXME: look for start of array, then check and strip quote if there, etc.
+  ;; - currently incorrectly assumes that * is wrapped in single quotes
+  ;; -- I have seen it both ways, not sure which one is correct
   (if (string/includes? path-str "['")
     (let [[path v*] (string/split path-str #"\['")
-          [v _] (string/split v* #"\']")]
-      {:path path :nested v})
+          [v _ :as around-vec] (string/split v* #"\']")
+          after-vec            (h/butfirst around-vec)]
+      (if (seq after-vec)
+        {:path path :nested v :rest after-vec}
+        {:path path :nested v}))
     {:path path-str}))
+
+;; TODO: mini dsl based on :path, :nested, :rest
+;; - :nested = * ~ mapv
+;;           = 'some-iri' ~ filterv (fn [{:keys [id]}] (= id some-iri))
+;; - :rest = navigation into object when :nested = some-iri
+;;         = navigate into every object
 
 (comment
   (= (handle-json-path-str "$.context.contextActivities.category['https://w3id.org/xapi/catch/v1']")
      {:path "$.context.contextActivities.category"
       :nested "https://w3id.org/xapi/catch/v1"})
+  (= (handle-json-path-str "$.attachments['*'].usageType")
+     {:path   "$.attachments"
+      :nested "*"
+      :rest [".usageType"]})
+  (= (handle-json-path-str "$.attachments['*'].usageType.dummyChild")
+     {:path   "$.attachments"
+      :nested "*"
+      :rest [".usageType.dummyChild"]})
   (= (handle-json-path-str "$.context.contextActivities.category['*']")
      {:path "$.context.contextActivities.category"
       :nested "*"})
@@ -32,9 +52,11 @@
 
 (defn deconstruct-json-path
   "ensure root was $ before returning the path into stmt"
-  [path]
-  (let [[root & components :as deconstructed] (string/split path #"\.")]
+  [path & {:keys [remainder]}]
+  (let [[root & components :as deconstructed] (string/split path #"\.")
+        within-array-path (when (seq remainder) (string/split remainder #"\."))]
     (assert (= root "$") "JSONPath string did not start with root!")
+    ;; FIXME: figure out simple/clean way to talk about within array paths!
     (h/butfirst deconstructed)))
 
 (comment
