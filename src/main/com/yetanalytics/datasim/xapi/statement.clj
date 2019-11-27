d78ed37449044456a6df67c47b2176bb4ae6b0b6
William Hoyt <will@yetanalytics.com>
2019-11-26 20:07:02 -0500

    semi-stub fn for following a statement path derived from location json-path string
    [DS-19]

--- src/main/com/yetanalytics/datasim/xapi/statement.clj
+++ src/main/com/yetanalytics/datasim/xapi/statement.clj
@@ -334,7 +334,33 @@
   ;; FIXME: top level impl
   )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Statement Path
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn follow-stmt-path
+  [stmt-path & {:keys [] :as passdown}]
+  (let [[top-lvl-k] stmt-path
+        ?more       (not-empty (everything-but-first stmt-path))]
+    (case top-lvl-k
+      "id"          "FIXME: handle presence + any/all/none"
+      "actor"       "FIXME: ?more is relevant"
+      "verb"        "FIXME: ?more is relevant"
+      "object"      "FIXME: ?more is relevant"
+      "result"      "FIXME: ?more is relevant"
+      "context"     "FIXME: ?more is relevant"
+      "timestamp"   "FIXME: handle presence + any/all/none"
+      "stored"      "FIXME: handle presence + any/all/none"
+      "authority"   (throw (ex-info "authority is not currently supported!"
+                                    {:stmt-path stmt-path}))
+      "version"     (throw (ex-info "version is not currently supported!"
+                                    {:stmt-path stmt-path}))
+      "attachments" (throw (ex-info "attachments are not currently supported!"
+                                    {:stmt-path stmt-path}))
+      (throw (ex-info "unexpected JSONPath key after root!"
+                      {:path       stmt-path
+                       :after-root top-lvl-k
+                       :more       ?more})))))
 
 
   )
