316e7257b441f663409656e24455e25e1e95448c
William Hoyt <will@yetanalytics.com>
2019-11-29 16:02:35 -0500

    moved functionality down into from-rule + new stub whose intended functionality is decribed by comments
    [DS-19]

--- src/main/com/yetanalytics/datasim/xapi/statement/rules.clj
+++ src/main/com/yetanalytics/datasim/xapi/statement/rules.clj
@@ -1,5 +1,6 @@
 (ns com.yetanalytics.datasim.xapi.statement.rules
-  (:require [com.yetanalytics.datasim.xapi.statement.helpers :as h]
+  (:require [clojure.set :as cset]
+            [com.yetanalytics.datasim.xapi.statement.helpers :as h]
             [com.yetanalytics.datasim.xapi.statement.location :as loc]
             [com.yetanalytics.datasim.xapi.statement.json-path :as jpath]
             [com.yetanalytics.datasim.xapi.statement.matchable :as matchable]))
@@ -23,159 +24,44 @@
      (continue-given-presence? nil)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Priority determination
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn handle-matchables
-  "helper fn which returns the stmt value to use for the current rule
-   - `matchable` = any/all/none
-   - `generated` = based on `location`
-   - `within-path` = item within `location` array, ie. ...['within-path']
-   - `iri-lookup` = iri-map from input"
-  [{:keys [matchable generated within-path iri-lookup]}]
-  (let [in-path-fn (fn [fallback]
-                     (if-some [_ within-path]
-                       ;; non-nil
-                       (if (string? within-path)
-                         ;; string
-                         (case within-path
-                           "*" fallback
-                           ""  fallback
-                           ;; assume IRI but return `within-path` + `fallback` if assumption is wrong
-                           (h/iri-lookup-attempt
-                            {:maybe-iri within-path
-                             :iri-map   iri-lookup
-                             :fallback  fallback}))
-                         ;; some non-string, unexpected, return fallback
-                         fallback)
-                       ;; nil, return fallback
-                       fallback))]
-    ;; check `matchable` first to see if profile does the work for us
-    (if-some [any-all-none (try (not-empty matchable)
-                                (catch Exception e matchable))]
-      ;; can be a fn or data
-      (let [matches (if (fn? any-all-none)
-                      ;; (->> none-coll (handle-none generated) (handle-any rng))
-                      ;; - see `matchable/compound-logic`
-                      (any-all-none generated)
-                      any-all-none)]
-        ;; attempt lookup, fallback to `matches`
-        (in-path-fn matches))
-      ;; attempt lookup, fallback to `generated`
-      (in-path-fn generated))))
-
-(comment
-  ;; `within-path` takes highest priority bc it CAN be a direct reference to something in profile
-  (= "test value"
-     (handle-matchables
-      {:matchable "matchable"
-       :generated "generated"
-       :within-path "mock-iri"
-       :iri-lookup {"mock-iri" "test value"}}))
-  ;; but if not, return `maybe-iri` + `fallback` with `matchable` taking higher priority then `generated`
-  (= {:non-iri "lookup miss"
-      :fallback "matchable"}
-     (handle-matchables
-      {:matchable "matchable"
-       :generated "generated"
-       :within-path "lookup miss"
-       :iri-lookup {"mock-iri" "test value"}}))
-  ;; but when `matchable` is not usable, fallback to `generated`
-  (= {:non-iri "lookup miss"
-      :fallback "generated"}
-     (handle-matchables
-      {:matchable nil
-       :generated "generated"
-       :within-path "lookup miss"
-       :iri-lookup {"mock-iri" "test value"}})
-     (handle-matchables
-      {:matchable []
-       :generated "generated"
-       :within-path "lookup miss"
-       :iri-lookup {"mock-iri" "test value"}}))
-  ;; no special handling of `fallback`
-  (= {:non-iri "lookup miss"
-      :fallback []}
-     (handle-matchables
-      {:matchable []
-       :generated []
-       :within-path "lookup miss"
-       :iri-lookup {"mock-iri" "test value"}}))
-  ;; but when its *, "" or nil, next priority is `matchable`
-  (= "Generated"
-     (handle-matchables
-      {:matchable (fn [d] (string/capitalize d))
-       :generated "generated"
-       :within-path nil
-       :iri-lookup {}}))
-  (= "matchable"
-     (handle-matchables
-      {:matchable "matchable"
-       :generated "generated"
-       :within-path :unexpected
-       :iri-lookup {}})
-     (handle-matchables
-      {:matchable "matchable"
-       :generated "generated"
-       :within-path "*"
-       :iri-lookup {}})
-     (handle-matchables
-      {:matchable "matchable"
-       :generated "generated"
-       :within-path ""
-       :iri-lookup {}})
-     (handle-matchables
-      {:matchable "matchable"
-       :generated "generated"
-       :iri-lookup {}}))
-  ;; return of `generation` only happens when necessary
-  (= "generated"
-     (handle-matchables
-      {:matchable []
-       :generated "generated"
-       :within-path nil
-       :iri-lookup {}})
-     (handle-matchables
-      {:matchable nil
-       :generated "generated"
-       :within-path nil
-       :iri-lookup {}})
-     (handle-matchables
-      {:matchable ""
-       :generated "generated"
-       :within-path nil
-       :iri-lookup {}})
-     (handle-matchables
-      {:matchable nil
-       :generated "generated"
-       :within-path :unexpected
-       :iri-lookup {}})
-     (handle-matchables
-      {:generated "generated"
-       :within-path "*"
-       :iri-lookup {}})))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Per rule
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn matchable-values
+(comment
+  ;; possible key within `any-all-none`
+  :must-nots
+  ;; -- TODO: impl of fn which takes the things it MUST NOT BE and infers what could be
+  ;; --- should be very similar to `follow-stmt-path` but based on
+  ;; ---- `:stmt-path`, `:within-stmt`, `:within-path`, `:any-all-none` and `:iri-lookup`
+  :all-of
+  ;; no need to generate, we have our value
+  :could-be
+  ;; rng selection from possibilities complete, just need to decide if we want to use the value at `could-be`
+  ;; - TODO: choice between `within-stmt` and the returned scalar value `any-all-none`
+  :generated
+  ;; the rule didn't provide anything for us to use, make decision based off of
+  ;; - `:stmt-path`, `:within-stmt`, `within-path`, `iri-lookup`
+  )
+
+(comment
+  ;; `matchable-values` needs to account for a few things
+  ;; - `ref-resolve` has 4 possible returns
+  ;; -- `:splat` = coll of things found `at-path`, ie. {`:splat` `at-path`}
+  ;; -- `:non-iri` = key queried for but nil was returned, ie. {`:non-iri` `nested`}
+  ;; -- map not wrapped in `:splat` or `:non-iri`, indication of successful lookup from `iri-map`, profile thing for `nested` returned
+  ;; -- thrown error, `nested` was non-nil, non-string, unexpected!
+  ;; - `any-all-none` has 4 possible returns
+  ;; -- `:could-be` = scalar or vector value which is approp for stmt based on any/all/none
+  ;; -- `:must-nots` = vector of values which CAN NOT be at `path`
+  ;; -- `:all-of` = vector of items which must be found at `path`
+  ;; -- `:generated` = any/all/none didn't provide any useful info, the generated values are returned
+  )
+
+(defn derive-stmt-val
   "handles interpretation of matchable values defined by the rule"
-  [{:keys [matchable generated within-path iri-lookup stmt-path]}]
-  (let [matched-values (handle-matchables
-                        {:matchable   matchable
-                         :generated   generated
-                         :within-path within-path
-                         :iri-lookup  iri-lookup})
-        non-iri?       (or (try (contains? matched-values :fallback)
-                                (catch Exception e false))
-                           (try (contains? matched-values :non-iri)
-                                (catch Exception e false)))]
-    (if non-iri?
-      (let [{:keys [non-iri fallback]} matched-values]
-        "TODO: figure out which is used as stmt value based on stmt-path")
-      (let [{component-type :type :as profile-item} matched-values]
-        "TODO: figure out what needs to be parsed from profile item based on stmt-path"))))
+  [{:keys [stmt-path within-stmt within-path any-all-none resolved-ref iri-lookup]}]
+  ;; FIXME: impl based on comments above
+  )
 
 (defn from-rule
   "top level fn that returns `stmt-path` and `stmt-val`
