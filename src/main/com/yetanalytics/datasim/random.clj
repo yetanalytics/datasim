d26d5268f3768742940aa3ee94919b22010839f2
Milt Reder <milt@yetanalytics.com>
2019-12-30 18:07:48 -0500

    gen tests for random

--- src/main/com/yetanalytics/datasim/random.clj
+++ src/main/com/yetanalytics/datasim/random.clj
@@ -1,12 +1,32 @@
 (ns com.yetanalytics.datasim.random
   "Seeded random functions"
-  (:require [com.yetanalytics.datasim.util.maths :as maths])
+  (:require [com.yetanalytics.datasim.util.maths :as maths]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as sgen])
   (:import [java.util
             UUID Random]))
+
+(s/def ::seed int?)
+
+(s/def ::rng
+  (s/with-gen #(instance? Random %)
+    (fn []
+      (sgen/fmap (fn [s]
+                   (Random. s))
+                 (s/gen ::seed)))))
+
+(s/fdef seed-rng
+  :args (s/cat :seed ::seed)
+  :ret ::rng)
 
 (defn seed-rng
   ^Random [^Long seed]
   (Random. seed))
+
+(s/fdef rand*
+  :args (s/cat :rng ::rng
+               :n (s/? number?))
+  :ret double?)
 
 (defn rand*
   (^Double [^Random rng]
@@ -14,13 +34,32 @@
   (^Double [^Random rng n]
    (* n (rand* rng))))
 
+(s/fdef rand-int*
+  :args (s/cat :rng ::rng
+               :n (s/int-in Integer/MIN_VALUE Integer/MAX_VALUE))
+  :ret number?)
+
 (defn rand-int*
   [rng n]
   (int (rand* rng n)))
 
+(s/fdef rand-nth*
+  :args (s/cat :rng ::rng
+               :coll (s/every any?
+                              :min-count 1))
+  :ret any?)
+
 (defn rand-nth*
   [rng coll]
   (nth coll (rand-int* rng (count coll))))
+
+(s/fdef shuffle*
+  :args (s/cat :rng ::rng
+               :coll (s/every any?))
+  :ret coll?
+  :fn (fn [{{:keys [rng coll cnt]} :args ret :ret}]
+        (= (set coll)
+           (set ret))))
 
 (defn shuffle*
   ([rng coll]
@@ -40,28 +79,50 @@
                (dec cnt))))))))
 
 
+(s/fdef random-sample*
+  :args (s/cat :rng ::rng
+               :prob (s/double-in :min 0.0 :max 1.0 :NaN? false)
+               :coll (s/every any? :min-count 1)
+               :weights (s/?
+                         (s/map-of any? double?)))
+  :ret coll?)
+
 (defn random-sample*
   ([rng prob]
-   (filter (fn [_] (< (rand rng) prob))))
+   (filter (fn [_] (< (rand* rng) prob))))
   ([rng prob coll]
-   (filter (fn [_] (< (rand rng) prob)) coll))
+   (filter (fn [_] (< (rand* rng) prob)) coll))
   ([rng prob coll weights]
    (filter (fn [el]
-             (< (rand rng)
+             (< (rand* rng)
                 (maths/min-max 0.0
                                (+ prob
                                   (get weights el 0.0))
                                1.0)))
            coll)))
 
+(s/fdef rand-gauss
+  :args (s/cat :rng ::rng
+               :mean double?
+               :sd double?)
+  :ret double?)
+
 (defn rand-gauss
   [^Random rng mean sd]
   (+ mean
      (* sd (.nextGaussian rng))))
 
+(s/fdef rand-long
+  :args (s/cat :rng ::rng)
+  :ret int?)
+
 (defn rand-long
   [^Random rng]
   (.nextLong rng))
+
+(s/fdef rand-uuid
+  :args (s/cat :rng ::rng)
+  :ret string?)
 
 (defn rand-uuid
   "Produce a random uuid (as a string) for the rng.
@@ -74,6 +135,17 @@
                (bit-or -9223372036854775808)
                (bit-and -4611686018427387905))]
     (.toString (UUID. x1 x2))))
+
+
+(s/def ::sd
+  (s/double-in :min 0.0 :infinite? false :NaN? false))
+
+(s/fdef choose
+  :args (s/cat :rng ::rng
+               :weights (s/map-of any? (s/double-in -1.0 1.0))
+               :coll (s/every any? :min-count 1)
+               :options (s/keys*
+                         :opt-un [::sd])))
 
 (defn choose
   [rng weights coll
