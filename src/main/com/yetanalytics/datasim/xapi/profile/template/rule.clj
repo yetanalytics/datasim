a2799fea70fcc78605b57f4ae4778f34725975a5
Milt Reder <milt@yetanalytics.com>
2020-01-09 18:54:52 -0500

    track modifications

--- src/main/com/yetanalytics/datasim/xapi/profile/template/rule.clj
+++ src/main/com/yetanalytics/datasim/xapi/profile/template/rule.clj
@@ -97,8 +97,8 @@
   a la https://github.com/adlnet/xapi-profiles/blob/master/xapi-profiles-communication.md#21-statement-template-validation.
   You can pass in matches for efficiency's sake."
   [statement
-   {:keys [location selector
-           any all none presence] :as rule}
+   {:keys                                                 [location selector
+                                                           any all none presence] :as rule}
    & [matches]]
   (let [strict (if (= presence "recommended")
                  false
@@ -180,40 +180,46 @@
    & {:keys [seed]}]
   (let [rng (random/seed-rng seed)]
     (loop [statement partial-statement
-           rules (map parse-rule
-                      raw-rules)]
+           rules     (map parse-rule
+                          raw-rules)
+           paths     {:applied #{}
+                      :excised #{}}]
       (if-let [{:keys [location selector
                        presence
                        any all none]
-                :as rule} (first rules)]
+                :as   rule} (first rules)]
         (let [matches (match-rule statement rule)]
           (if (follows-rule? statement rule matches)
             ;; if the statement follows the rule, continue processing!
-            (recur statement (rest rules))
+            (recur statement (rest rules) paths)
             ;; if not, we've got work to do..
             (if ;; the simplest case is an exclusion rule, which we can safely
                 ;; apply without additional recursion
                 (= presence "excluded")
-              (recur
-               (cond-> (json-path/excise statement location :prune-empty? true)
-                 selector
-                 (json-path/excise (into location selector) :prune-empty? true))
-               (rest rules))
+              (let [statement-excised (cond-> (json-path/excise statement location :prune-empty? true)
+                                        selector
+                                        (json-path/excise (into location selector) :prune-empty? true))]
+                (recur
+                 statement-excised
+                 (rest rules)
+                 (update paths :excised into (some-> statement-excised
+                                                     meta
+                                                     :paths))))
               ;; for other rules, we'll need to get fancier...
               (let [location-enum (json-path/enumerate location)
-                    xapi-spec (try (xp/path->spec
-                                    ::xs/statement
-                                    (first location-enum)
-                                    statement)
-                                   (catch java.lang.AssertionError ae
-                                     (throw (ex-info "Couldn't figure out xapi path"
-                                                     {:type ::undefined-path
-                                                      :key-path (first location-enum)
-                                                      :rule rule
-                                                      :statement statement}
-                                                     ae))))
+                    xapi-spec     (try (xp/path->spec
+                                        ::xs/statement
+                                        (first location-enum)
+                                        statement)
+                                       (catch java.lang.AssertionError ae
+                                         (throw (ex-info "Couldn't figure out xapi path"
+                                                         {:type      ::undefined-path
+                                                          :key-path  (first location-enum)
+                                                          :rule      rule
+                                                          :statement statement}
+                                                         ae))))
 
-                    xapi-gen (s/gen xapi-spec)
+                    xapi-gen      (s/gen xapi-spec)
                     xapi-rule-gen (rule-gen rule xapi-gen)
 
                     ;; TODO: remove unmatchable paths
@@ -222,51 +228,60 @@
                                        (filter (partial valid-value? rule)
                                                (remove #{::unmatchable} matches)))
 
-                    rule-or-val-gen (cond->> (gen/one-of (into [xapi-rule-gen]
-                                                               (map gen/return
-                                                                    valid-values)))
-                                      ;; must be in all if defined
-                                      all (gen/such-that (partial contains? all))
-                                      ;; must not be in none if defined
-                                      none (gen/such-that (complement (partial contains? none))))
-                    values-gen (cond->> (if (json-path/discrete? location)
-                                          (let [path-count (count location-enum)]
-                                            (if (= 1 path-count)
-                                              (gen/tuple
-                                               (if any
-                                                 (gen/elements any)
-                                                 rule-or-val-gen))
-                                              (apply gen/tuple (repeat path-count
-                                                                       rule-or-val-gen))))
-                                          ;; deal with distinct arrays
-                                          (if (#{:definition/choices
-                                                 :definition/scale
-                                                 :definition/source
-                                                 :definition/target
-                                                 :definition/steps} xapi-spec)
-                                            (gen/vector-distinct rule-or-val-gen
-                                                                 {:min-elements 1
-                                                                  :max-elements (or (and all)
-                                                                                    (count all))})
-                                            (gen/vector rule-or-val-gen 1 10)))
-                                 ;; at least 1 any
-                                 any (gen/such-that (partial some (partial contains? any))))]
+                    rule-or-val-gen   (cond->> (gen/one-of (into [xapi-rule-gen]
+                                                                 (map gen/return
+                                                                      valid-values)))
+                                        ;; must be in all if defined
+                                        all  (gen/such-that (partial contains? all))
+                                        ;; must not be in none if defined
+                                        none (gen/such-that (complement (partial contains? none))))
+                    values-gen        (cond->> (if (json-path/discrete? location)
+                                                 (let [path-count (count location-enum)]
+                                                   (if (= 1 path-count)
+                                                     (gen/tuple
+                                                      (if any
+                                                        (gen/elements any)
+                                                        rule-or-val-gen))
+                                                     (apply gen/tuple (repeat path-count
+                                                                              rule-or-val-gen))))
+                                                 ;; deal with distinct arrays
+                                                 (if (#{:definition/choices
+                                                        :definition/scale
+                                                        :definition/source
+                                                        :definition/target
+                                                        :definition/steps} xapi-spec)
+                                                   (gen/vector-distinct rule-or-val-gen
+                                                                        {:min-elements 1
+                                                                         :max-elements (or (and all)
+                                                                                           (count all))})
+                                                   (gen/vector rule-or-val-gen 1 10)))
+                                        ;; at least 1 any
+                                        any (gen/such-that (partial some (partial contains? any))))
+                    statement-applied (json-path/apply-values
+                                       statement location
+                                       (try (gen/generate values-gen 30 seed)
+                                            (catch clojure.lang.ExceptionInfo exi
+                                              (throw (ex-info "Generation error!"
+                                                              {:type            ::gen-error
+                                                               :rule            rule
+                                                               :statement       statement
+                                                               :matched         matches
+                                                               :rule-or-val-gen rule-or-val-gen
+                                                               :values-gen      values-gen
+                                                               :seed            seed
+                                                               }
+                                                              exi)))))]
                 (recur
-                 (json-path/apply-values statement location
-                                         (try (gen/generate values-gen 30 seed)
-                                              (catch clojure.lang.ExceptionInfo exi
-                                                (throw (ex-info "Generation error!"
-                                                                {:type ::gen-error
-                                                                 :rule rule
-                                                                 :statement statement
-                                                                 :matched matches
-                                                                 :rule-or-val-gen rule-or-val-gen
-                                                                 :values-gen values-gen
-                                                                 :seed seed
-                                                                 }
-                                                                exi)))))
-
-
-                 (rest rules))))))
+                 statement-applied
+                 (rest rules)
+                 (update paths :applied into (some-> statement-applied
+                                                     meta
+                                                     :paths)))))))
         ;; all rules pass and we're done!
-        statement))))
+        ;; but not quite... we have to walk up removed/changed/added paths and
+        ;; check the specs
+        (if (s/valid? ::xs/statement statement)
+          statement
+          (throw (ex-info "Healing not yet implemented"
+                          {:type ::not-yet-implemented
+                           :statement-error (s/explain-data ::xs/statement statement)})))))))
