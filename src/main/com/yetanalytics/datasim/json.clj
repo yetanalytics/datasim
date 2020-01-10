608121e3724700400ae587f1cb0e595d77bd9c1f
Milt Reder <milt@yetanalytics.com>
2020-01-10 11:28:54 -0500

    some utils for working on clojure data you know is JSON

--- src/main/com/yetanalytics/datasim/json.clj
+++ src/main/com/yetanalytics/datasim/json.clj
@@ -26,3 +26,18 @@
                 :kind vector?
                 :into []
                 :gen-max 4)))))
+
+(defn jassoc
+  "Like assoc, but if the first arg is nil it will dispatch on key to create the
+   value, placing val in a new vector if needed. Only has the one simple arity"
+  [coll k v]
+  (if (or coll (string? k))
+    (assoc coll k v)
+    (assoc [] k v)))
+
+(defn jassoc-in
+  "like assoc-in, but for jassoc"
+  [m [k & ks] v]
+  (if ks
+    (jassoc m k (jassoc-in (get m k) ks v))
+    (jassoc m k v)))
