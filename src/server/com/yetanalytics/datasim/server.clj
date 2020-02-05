12bde6d451202cc57611304b3ac2e5601cba066b
Boris Boiko <bsb32494@gmail.com>
2020-02-05 18:46:34 -0500

    Added in deps to allow for LRS streaming. Still MVP iteration.

--- src/server/com/yetanalytics/datasim/server.clj
+++ src/server/com/yetanalytics/datasim/server.clj
@@ -3,7 +3,10 @@
             [io.pedestal.http                       :as http]
             [io.pedestal.http.route                 :as route]
             [io.pedestal.http.body-params           :as body-params]
+            [io.pedestal.http.ring-middlewares      :refer [multipart-params]]
             [clojure.data.json                      :as json]
+            [cheshire.core                          :as c]
+            [clj-http.client                        :as client]
             [com.yetanalytics.datasim.sim           :as sim]
             [com.yetanalytics.datasim.util.sequence :as su])
   (:import [javax.servlet ServletOutputStream])
@@ -16,6 +19,13 @@
   {:status 200
    :body   "OK"})
 
+(defn read-data
+  "Given a map and a key, retrieve the content of the key and read the JSON into EDN."
+  [m k]
+  (-> m
+      (get k)
+      (json/read-str :key-fn keyword)))
+
 (defn run-sim!
   "Returns a function that will accept an output stream to write to the client.
    Inside that function, a writer will open and the simulation will run.
@@ -23,48 +33,75 @@
   [input]
   ;; Anon fn that accepts the output stream for the response body.
   (fn [^ServletOutputStream os]
-    (with-open [w (writer os)]
-      ;; Iterate over the entire input skeleton, generate statements.
-      (doseq [s (-> (sim/build-skeleton input)
-                    vals
-                    (->> (su/seq-sort
-                          (comp :timestamp-ms
-                                meta))))]
-        ;; Write each statement to the stream, pad with newline at end.
-        (json/write s w
-                    :escape-slash   false
-                    :escape-unicode false)
-        (.write w "\n")))))
+    ;; Uses multipart message for the request.
+    ;; Read in each part of the input file, and convert into EDN
+    (let [data           {:profiles   (read-data input "profiles")
+                          :personae   (read-data input "personae")
+                          :alignments (read-data input "alignments")
+                          :parameters (read-data input "parameters")}
+          send-to-lrs    (or (get input "send-to-lrs") false)
+          endpoint       (get input "lrs-endpoint")
+          api-key        (get input "api-key")
+          api-secret-key (get input "api-secret-key")]
+      (with-open [w (writer os)]
+        ;; Iterate over the entire input skeleton, generate statements.
+        ;; Write them wrapped in a list
+        (.write w "[\n")
+        (doseq [s (-> (sim/build-skeleton data)
+                      vals
+                      (->> (su/seq-sort
+                            (comp :timestamp-ms
+                                  meta))))]
+          (try
+            (when send-to-lrs
+              ;; Stream statement to an LRS
+              (client/post endpoint
+                           {:basic-auth [api-key api-secret-key]
+                            :headers    {"X-Experience-API-Version" "1.0.3"
+                                         "Content-Type"             "application/json;"}
+                            :body       (c/generate-string s)}))
+            ;; Write each statement to the stream, pad with newline at end.
+            (json/write s w
+                        :escape-slash   false
+                        :escape-unicode false)
+            (.write w "\n")
+            (catch Exception e
+              (println "---- Error ----"))))
+        (.write w "]")))))
 
-(def run-sim
-  {:name  :datasim.route/run-sim
+(def generate
+  {:name  :datasim.route/generate
    :enter (fn [context]
             ;; Generate response to stream simulation to the client.
             ;; Return a json file, and set it to be downloaded by the user.
             (assoc context
                    :response
-                   {:status  200
-                    :headers {"Content-Type" "application/json"
-                              "Content-Disposition" (format "attachment; filename=\"simulation.json\"")}
-                    :body    (run-sim! (-> context
-                                           :request
-                                           :json-params))}))})
+                   (try
+                     {:status  200
+                      :headers {"Content-Type"        "application/json; charset=utf-8"
+                                "Content-Disposition" (format "attachment; filename=\"simulation.json\"")}
+                      :body    (run-sim! (-> context
+                                             :request
+                                             :multipart-params))}
+                     (catch Exception e
+                       {:status 400
+                        :body   (.getMessage e)}))))})
 
 (def routes
   (route/expand-routes
    #{["/health"
       :get        health
       :route-name :datasim.route/health]
-     ["/run-sim"
-      :post [(body-params/body-params)
-             run-sim]]}))
+     ["/api/v1/generate"
+      :post [(multipart-params)
+             generate]]}))
 
 (defn create-server
   []
   (http/create-server
    {::http/routes routes
     ::http/type   :immutant
-    ::http/port   9000
+    ::http/port   9090
     ::http/join?  false}))
 
 (defn start
