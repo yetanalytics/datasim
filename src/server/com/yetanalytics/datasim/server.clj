87b0b352ec11e4b7ccc07a923334a154038c0786
Boris Boiko <bsb32494@gmail.com>
2020-03-06 16:51:37 -0500

    Added in basic auth through Buddy.

--- src/server/com/yetanalytics/datasim/server.clj
+++ src/server/com/yetanalytics/datasim/server.clj
@@ -2,23 +2,26 @@
   (:require [clojure.java.io                        :refer [writer]]
             [io.pedestal.http                       :as http]
             [io.pedestal.http.route                 :as route]
-            [io.pedestal.http.body-params           :as body-params]
             [io.pedestal.http.ring-middlewares      :refer [multipart-params]]
+            [io.pedestal.interceptor                :as interceptor]
+            [io.pedestal.interceptor.chain          :as chain]
+            [io.pedestal.interceptor.error          :refer [error-dispatch]]
             [ring.util.codec                        :as codec]
+            [buddy.auth                             :as auth]
+            [buddy.auth.backends                    :as backends]
+            [buddy.auth.middleware                  :as middleware]
             [clojure.data.json                      :as json]
             [cheshire.core                          :as c]
             [clj-http.client                        :as client]
+            [environ.core                           :refer [env]]
             [com.yetanalytics.datasim.sim           :as sim]
             [com.yetanalytics.datasim.util.sequence :as su])
   (:import [javax.servlet ServletOutputStream])
   (:gen-class))
 
-(defn health
-  "Route implementation for the health endpoint.
-   Currently it just returns a 200 OK if the server is up and running."
-  [request]
-  {:status 200
-   :body   "OK"})
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Datasim fns
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn read-data
   "Given a map and a key, retrieve the content of the key and read the JSON into EDN."
@@ -70,23 +73,90 @@
               (println "---- Error ----"))))
         (.write w "]")))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Auth data and fns
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Reach into the environment and grab all of the user credentials from there
+;;  that will be used in basic auth.
+(defonce users
+  (let [credentials (env :credentials)
+        users       (clojure.string/split credentials #";")]
+    ;; Create a map of every allowed credential
+    (reduce (fn [m cred]
+              (let [[user pass] (clojure.string/split cred #":")]
+                (assoc m
+                       user
+                       {:username user
+                        :password pass})))
+            {}
+            users)))
+
+(defn auth-fn
+  "This function will ensure that the creds from Basic Auth are authenticated."
+  [request {:keys [username password]}]
+  (when-let [user (get users username)]
+    (when (= password (:password user))
+      username)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Interceptors/Middleware
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; A wrapper for Buddy to know what the authentication fn is
+(def backend 
+  (backends/basic {:realm  "MyApi"
+                   :authfn auth-fn}))
+
+(def authentication-interceptor
+  "Port of buddy-auth's wrap-authentication middleware."
+  (interceptor/interceptor
+   {:name ::authenticate
+    :enter (fn [ctx]
+             (update ctx :request middleware/authentication-request backend))}))
+
+(defn authorization-interceptor
+  "Port of buddy-auth's wrap-authorization middleware."
+  [backend]
+  (error-dispatch [ctx ex]
+                  [{:exception-type :clojure.lang.ExceptionInfo :stage :enter}]
+                  (try
+                    (assoc ctx
+                           :response
+                           (middleware/authorization-error (:request ctx)
+                                                           ex
+                                                           backend))
+                    (catch Exception e
+                      (assoc ctx ::chain/error e)))
+                  :else (assoc ctx ::chain/error ex)))
+
 (def generate
   {:name  :datasim.route/generate
    :enter (fn [context]
             ;; Generate response to stream simulation to the client.
             ;; Return a json file, and set it to be downloaded by the user.
-            (assoc context
-                   :response
-                   (try
-                     {:status  200
-                      :headers {"Content-Type"        "application/json; charset=utf-8"
-                                "Content-Disposition" (format "attachment; filename=\"simulation.json\"")}
-                      :body    (run-sim! (-> context
-                                             :request
-                                             :multipart-params))}
-                     (catch Exception e
-                       {:status 400
-                        :body   (.getMessage e)}))))})
+            (if (auth/authenticated? (:request context))
+              (assoc context
+                     :response
+                     (try
+                       {:status  200
+                        :headers {"Content-Type"        "application/json; charset=utf-8"
+                                  "Content-Disposition" (format "attachment; filename=\"simulation.json\"")}
+                        :body    (run-sim! (-> context
+                                               :request
+                                               :multipart-params))}
+                       (catch Exception e
+                         {:status 400
+                          :body   (.getMessage e)})))
+              (if (get (-> context :request :headers) "authorization")
+                (assoc context
+                       :response
+                       {:status 403
+                        :body   "Not Authenticated"})
+                (assoc context
+                       :response
+                       {:status 401
+                        :body   "No Authorization"}))))})
 
 (def download-url
   {:name  :datasim.route/download-url
@@ -115,16 +185,37 @@
                        {:status 501
                         :body   "other"}))))})
 
+(def common-interceptors
+  [authentication-interceptor
+   (authorization-interceptor backend)])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Route implementations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn health
+  "Route implementation for the health endpoint.
+   Currently it just returns a 200 OK if the server is up and running."
+  [request]
+  {:status 200
+   :body   "OK"})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Routes and server configs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def routes
   (route/expand-routes
    #{["/health"
       :get        health
       :route-name :datasim.route/health]
      ["/api/v1/generate"
-      :post [(multipart-params)
-             generate]]
+      :post (into common-interceptors
+                  [(multipart-params)
+                   generate])]
      ["/api/v1/download-url"
-      :get  [download-url]]}))
+      :get  (into common-interceptors
+                  [download-url])]}))
 
 (defn create-server
   []
