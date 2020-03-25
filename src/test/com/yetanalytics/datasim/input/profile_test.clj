89e57cc8fe967a3449c75fcf7417023681c29397
Milt Reder <milt@yetanalytics.com>
2020-03-25 12:46:48 -0400

    added test for valid record output

--- src/test/com/yetanalytics/datasim/input/profile_test.clj
+++ src/test/com/yetanalytics/datasim/input/profile_test.clj
@@ -2,11 +2,29 @@
   (:require [clojure.test :refer :all]
             [com.yetanalytics.datasim.protocols :as p]
             [com.yetanalytics.datasim.input.profile :refer :all]
+            [com.yetanalytics.pan.objects.profile :as profile]
+            [clojure.spec.alpha :as s]
             [com.yetanalytics.datasim.io :as dio]
-            [clojure.template :as t]))
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]
+            [clojure.template :as t])
+  (:import [java.io File]))
 
 (deftest minimal-profile-test
   (let [minimal-profile (dio/read-loc-json (map->Profile {})
                                            "dev-resources/profiles/minimal.jsonld")]
     (testing "is valid"
-      (is (nil? (p/validate minimal-profile))))))
+      (is (nil? (p/validate minimal-profile))))
+    (testing "is valid when written"
+      (let [^File tf (File/createTempFile "profiletest" nil)]
+        (try
+          (dio/write-loc-json minimal-profile tf)
+          (is
+           (nil?
+            (s/explain-data ::profile/profile
+                            (json/read-str
+                             (slurp tf)
+                             :key-fn
+                             (partial p/read-key-fn minimal-profile)))))
+          (finally
+            (.delete tf)))))))
