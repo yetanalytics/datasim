a93c3e4f5df6907044e226c6b106ed8d00a06375
Milt Reder <milt@yetanalytics.com>
2020-03-30 13:02:36 -0400

    updated bench with a simple count fn and some results

--- src/dev/bench.clj
+++ src/dev/bench.clj
@@ -1,27 +1,42 @@
 (ns bench
   (:require [criterium.core :as c]
             [com.yetanalytics.datasim.sim :as sim]
-            [com.yetanalytics.datasim.input :as input]
-            [com.yetanalytics.datasim.util.sequence :as su]))
+            [com.yetanalytics.datasim.input :as input]))
 
-
-(def input
-  (input/map->Input
-   {:profiles [(input/from-location :profile :json "dev-resources/bench/calibration.jsonld")]
-    :personae (input/from-location :personae :json "dev-resources/bench/actors.json")
-    :alignments (input/from-location :alignments :json "dev-resources/bench/alignments.json")
-    :parameters (input/from-location :parameters :json "dev-resources/bench/params.json")}))
+(defn statements-per-second
+  "How many statements per second will datasim generate?"
+  [input]
+  (let [sim-seq (sim/sim-seq input)
+        t-start (System/currentTimeMillis)
+        t-end (+ t-start 1000)]
+    (loop [[_ & rest-s] sim-seq
+           s-count 0]
+      (if (> t-end (System/currentTimeMillis))
+        (recur rest-s (inc s-count))
+        s-count))))
 
 (comment
+  (def input
+    (input/map->Input
+     {:profiles [(input/from-location :profile :json "dev-resources/bench/calibration.jsonld")]
+      :personae (input/from-location :personae :json "dev-resources/bench/actors.json")
+      :alignments (input/from-location :alignments :json "dev-resources/bench/alignments.json")
+      :parameters (input/from-location :parameters :json "dev-resources/bench/params.json")}))
 
   (c/with-progress-reporting
-    (c/quick-bench (do (-> (sim/build-skeleton input)
-                           vals
-                           (->> (su/seq-sort
-                                 (comp :timestamp-ms
-                                       meta))
-                                (take 1000)
-                                (into [])))
+    (c/quick-bench (do (doall (take 1000 (sim/sim-seq input)))
                        nil)))
+
+  ;; Evaluation count : 6 in 6 samples of 1 calls.
+  ;; Execution time mean : 201.061588 ms
+  ;; Execution time std-deviation : 6.698801 ms
+  ;; Execution time lower quantile : 196.857258 ms ( 2.5%)
+  ;; Execution time upper quantile : 212.503809 ms (97.5%)
+  ;; Overhead used : 7.430081 ns
+
+  (statements-per-second input) ;; => 4938
+
+  (quot (reduce + (repeatedly 60 #(statements-per-second input)))
+        60) ;; => 4462
 
   )
