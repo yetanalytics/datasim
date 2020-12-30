ee7082e54207ad7c2d2f17b0ad96d1c7c2712801
Milt Reder <milt@yetanalytics.com>
2020-12-29 23:06:14 -0500

    only retain count of ids

--- src/main/com/yetanalytics/datasim/xapi/client.clj
+++ src/main/com/yetanalytics/datasim/xapi/client.clj
@@ -11,15 +11,17 @@
              "content-type" "application/json"}})
 
 (defn post-statements
-  "Given LRS options and a seq of statements, send them to an LRS in sync batches"
+  "Given LRS options and a seq of statements, send them to an LRS in sync batches
+  If an emit-ids-fn is given it will be called with posted statement ids on success."
   [{:keys [endpoint
            batch-size
            http-options]
     :or {batch-size 10}}
-   statement-seq]
+   statement-seq
+   & {:keys [emit-ids-fn]}]
   ;; TODO: Exp backoff, etc
   (loop [batches (partition-all batch-size statement-seq)
-         success []
+         success 0
          fail []]
     (if-let [batch (first batches)]
       (let [{:keys [status body] :as response} @(http/post
@@ -29,14 +31,16 @@
                                                         {:body (json/encode batch)
                                                          :as :stream}))]
         (if (= 200 status)
-          (recur (rest batches)
-                 (into success
-                       (map
-                        (fn [^String id]
-                          (java.util.UUID/fromString id))
-                        (with-open [rdr (io/reader body)]
-                         (json/decode-stream rdr))))
-                 fail)
+          (let [ids (map
+                     (fn [^String id]
+                       (java.util.UUID/fromString id))
+                     (with-open [rdr (io/reader body)]
+                       (json/decode-stream rdr)))]
+            (when emit-ids-fn
+              (emit-ids-fn ids))
+            (recur (rest batches)
+                   (+ success (count ids))
+                   fail))
           {:success success
            :fail (conj fail
                        (cond-> response
