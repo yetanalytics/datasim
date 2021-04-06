305936050e2ce5d1366fc2c3aedf78df4550d240
Milt Reder <milt@yetanalytics.com>
2021-04-06 19:04:10 -0400

    DS-102 my own version of the scheduler

---
+++ src/onyx/com/yetanalytics/datasim/onyx/scheduling/semi_colocated_task_scheduler.clj
@@ -0,0 +1,115 @@
+(ns com.yetanalytics.datasim.onyx.scheduling.semi-colocated-task-scheduler
+  (:require [onyx.scheduling.common-task-scheduler :as cts]
+            [onyx.scheduling.common-job-scheduler :as cjs]
+            [onyx.static.util :refer [index-of]]
+            [onyx.log.commands.common :as common]
+            [onyx.scheduling.colocated-task-scheduler :as colo]
+            [clojure.set :as cset])
+  (:import [org.btrplace.model.constraint Fence SplitAmong Ban]))
+
+;; Only ever gives one peer to a task
+(defmethod cts/task-distribute-peer-count :onyx.task-scheduler/semi-colocated
+  [replica job n]
+  (let [tasks (get-in replica [:tasks job])
+        t (cjs/job-lower-bound replica job)]
+    (if (< n t)
+      (zipmap tasks (repeat 0))
+      ;; one peer to a task
+      (zipmap tasks (repeat 1)))))
+
+
+
+(defn group-tasks
+  "Greedily partition tasks by i/o"
+  [in->out job-id]
+  (reduce-kv
+   (fn [groups in-task out-tasks]
+     (if-let [applicable-group (some (fn [g]
+                                       (when
+                                           (or
+                                            ;; in task in other groups (an branch task)
+                                            (contains? g in-task)
+                                            ;; out task in other groups
+                                            (not-empty
+                                             (cset/intersection
+                                              out-tasks
+                                              g)))
+                                         g))
+                                     groups)]
+       ;; join
+       (-> groups
+           (disj applicable-group)
+           (conj (into applicable-group
+                       (cons in-task
+                             out-tasks))))
+       ;; new group
+       (conj groups
+             (into #{}
+                   (cons in-task
+                         out-tasks)))))
+   #{}
+   (get in->out job-id)))
+
+
+;; All tasks should be bounded, or they get a sat of 1
+(defmethod cts/task-constraints :onyx.task-scheduler/semi-colocated
+  [{:keys [groups-index
+           in->out
+           saturation ;; total
+           task-saturation ;; all jorbs
+           ]
+    :as replica} jobs task-capacities peer->vm task->node no-op-node job-id]
+  (let [job-saturation (get saturation job-id)
+        task-saturations (get task-saturation job-id)
+        task-ids (get-in replica [:tasks job-id])
+        ;; pull out physical groupings
+        group->peer->vm
+        (into {}
+              (for [[g ps] groups-index]
+                [g
+                 (select-keys peer->vm ps)]))
+        ;; greedily group task io into groups
+        task-groups
+        (group-tasks
+         in->out job-id)
+        ;; here this is the min group size to run an i/o group
+        ;; if it's infinity this won't ever work
+        capacity (apply max
+                         0
+                         (map (fn [task-group]
+                                (reduce
+                                 +
+                                 (map
+                                  #(get task-saturations % 1)
+                                  task-group)))
+                              task-groups))
+        suitable-groups (keep
+                         (fn [[g ps]]
+                           (when (<= capacity (count ps))
+                             g))
+                         groups-index)
+        ret
+        ;; if we can cover all tasks
+        (if (<=
+             (count task-groups)
+             (count suitable-groups))
+          (into []
+                (map
+                 (fn [[g pvm] task-group]
+                   (SplitAmong.
+                    [(vals pvm)]
+                    [(mapv
+                      #(get task->node [job-id %])
+                      task-group)]))
+                 group->peer->vm
+                 task-groups))
+          ;; Can't fit? BAN EVERYTHING
+          (into []
+                (Ban/newBan (vals peer->vm) (vals task->node))))]
+
+    ret))
+
+
+(defmethod cts/assign-capacity-constraint? :onyx.task-scheduler/semi-colocated
+  [replica job-id]
+  true)
---
+++ src/test/com/yetanalytics/datasim/onyx/scheduling/semi_colocated_task_scheduler_test.clj
@@ -0,0 +1,141 @@
+(ns com.yetanalytics.datasim.onyx.scheduling.semi-colocated-task-scheduler-test
+  (:require [clojure.test :refer :all]
+            [onyx.scheduling.common-job-scheduler :refer [reconfigure-cluster-workload]]
+            [onyx.api]
+            com.yetanalytics.datasim.onyx.scheduling.semi-colocated-task-scheduler))
+
+;; From onyx.log.generators
+
+(defn one-group [replica]
+  (-> replica
+      (assoc :groups [:g1])
+      (assoc-in [:groups-index :g1] (into #{} (:peers replica)))
+      ((fn [rep]
+         (reduce
+          #(assoc-in %1 [:groups-reverse-index %2] :g1)
+          rep
+          (:peers rep))))))
+
+(defn generate-group-and-peer-ids
+  ([groups peers]
+   (generate-group-and-peer-ids 1 groups
+                                1 peers))
+  ([groups-low groups-high peers-low peers-high]
+   (reduce
+    (fn [r g]
+      (let [g-id (keyword (str "g" g))
+            peers (map
+                   #(keyword (str (name g-id) "-p" %))
+                   (range peers-low (+ peers-low peers-high)))]
+        (assoc r g-id (into #{} peers))))
+    {}
+    (range groups-low (+ groups-low groups-high)))))
+
+(defn gen-replica
+  [groups peers]
+  (let [group-map (generate-group-and-peer-ids groups peers)]
+    {
+
+     :peers
+     (into []
+           (mapcat
+            seq
+            (vals group-map))),
+     :peer-tags
+     (into {}
+           (for [[g ps] group-map
+                 p ps]
+             [p []]))
+     :groups-index group-map,
+     :peer-sites
+     (into {}
+           (for [[g ps] group-map
+                 p ps]
+             [p
+              {:address (format "%s-addr"
+                                (name g)
+                                )
+               :port 40200}])),
+     :groups (into [] (keys group-map)),
+     :groups-reverse-index
+     (into {}
+           (for [[g ps] group-map
+                 p ps]
+             [p g])),
+     :pairs (into {}
+                  (mapcat identity
+                          (for [[g0 _]  group-map
+                                [g1 _]  group-map
+                                :when (not= g0 g1)]
+                            [[g0 g1]
+                             [g1 g0]])
+                          #_(for [[[g0 _] [g1 _]] (partition 2 group-map)]
+                            [[g0 g1]
+                             [g1 g0]]))),
+     :orphaned-peers {},
+     :output-tasks {},
+     :grouped-tasks {},
+     :job-scheduler :onyx.job-scheduler/balanced,
+     :saturation {},
+     :in->out {},
+     :task-percentages {},
+     :min-required-peers {},
+     :state-tasks {},
+     :task-slot-ids {},
+     :accepted {},
+     :aborted #{},
+     :jobs [],
+     :tasks {},
+     :task-metadata {},
+     :allocation-version {},
+     :required-tags {},
+     :flux-policies {},
+     :messaging #:onyx.messaging{:impl :aeron},
+     :coordinators {},
+     :reduce-tasks {},
+     :allocations {},
+     :killed-jobs [],
+     :prepared {},
+     :percentages {},
+     :input-tasks {},
+     :message-short-ids {},
+     :version 7,
+     :completed-jobs [],
+     :log-version "0.14.6-SNAPSHOT",
+     :task-schedulers {},
+     :left #{},
+     :task-saturation {}
+     }))
+
+
+(deftest semi-colocated-colocates-workflow-edges
+  (is
+   (=
+    {:j1 {:in-1 [:g1-p2],
+          :out-1 [:g1-p1],
+          :out-2 [:g2-p1],
+          :in-2 [:g2-p2],
+          :out-0 [:g3-p2],
+          :in-0 [:g3-p1]}}
+    (let [old (gen-replica
+               3 ;; three physical
+               2) ;; two vpeers each
+
+          new (merge old
+                     {:task-schedulers {:j1 :onyx.task-scheduler/semi-colocated}
+                      :jobs [:j1]
+                      :tasks {:j1 [:in-0 :out-0
+                                   :in-1 :out-1
+                                   :in-2 :out-2]}
+                      :saturation {:j1 6}
+                      :task-saturation {:j1 {:in-0 1
+                                             :out-0 1
+                                             :in-1 1
+                                             :out-1 1
+                                             :in-2 1
+                                             :out-2 1}}
+                      :in->out {:j1 {:in-0 #{:out-0}
+                                     :in-1 #{:out-1}
+                                     :in-2 #{:out-2}}}
+                      })]
+      (:allocations (reconfigure-cluster-workload new old))))))
