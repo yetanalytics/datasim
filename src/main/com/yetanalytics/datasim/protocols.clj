d3a8768dc0b14d2edaa4f21f1fe15e0fff8accde
kelvinqian00 <kelvinqian2@gmail.com>
2023-05-02 16:24:11 -0400

    Update protocol comments and docstrings

--- src/main/com/yetanalytics/datasim/protocols.clj
+++ src/main/com/yetanalytics/datasim/protocols.clj
@@ -1,23 +1,26 @@
 (ns com.yetanalytics.datasim.protocols
-  "Protocols for use across the application, we enumerate them here in one NS so
-   we don't get into circ. dependency errors.")
+  "Protocols for use across the application,")
+
+;; We enumerate them here in one namespace so that we don't get into circular
+;; dependency errors.
 
 (defprotocol FromInput
-  "Things that come from the user and must be validated."
-  (validate [this]
-    "Validate the input, which must be read in first. Returns nil if the input
-     is valid, or a vector of error maps each containing:
-       :id - Generated unique ID for error
-       :path - Path of error in input
-       :text - Error message/text"))
+  "Inputs that come from CLI a file, or other source and must bevalidated."
+  (validate [input]
+    "Validate the `input`, which must be read in first. Returns `nil` if the
+     `input` is valid, or a vector of error maps each containing:
+     
+     - `:id` - Generated unique ID for error
+     - `:path` - Path of error in input
+     - `:text` - Error message/text"))
 
 (defprotocol JSONRepresentable
-  "Things that can be represented as JSON"
+  "Things that can be represented as JSON."
   (read-key-fn [this k]
-    "Wrapped in a partial, will coerce k to internal representation.")
+    "Wrapped in a partial, will coerce key `k` to internal representation.")
   (read-body-fn [this json-result]
     "Return a new record with the given JSON incorporated")
   (write-key-fn [this k]
-    "Wrapped in a partial, will coerce k to json representation")
+    "Wrapped in a partial, will coerce key `k` to JSON representation")
   (write-body-fn [this]
-    "Return the part of the record used to generate json."))
+    "Return the part of the record used to generate JSON."))
