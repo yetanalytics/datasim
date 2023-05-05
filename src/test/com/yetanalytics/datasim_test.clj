b0dc34e9698e3802f54ff509379e197b02e598f7
kelvinqian00 <kelvinqian2@gmail.com>
2023-05-05 16:40:34 -0400

    Remove datasim-test ns

--- src/test/com/yetanalytics/datasim_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.yetanalytics.datasim-test
-  (:require [clojure.test :refer :all]
-            [com.yetanalytics.datasim :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is true #_(= 0 1))))
