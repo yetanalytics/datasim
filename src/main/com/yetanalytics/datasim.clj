55daa211bafba8eb809dbd82debea903dc87ac68
kelvinqian00 <kelvinqian2@gmail.com>
2023-07-05 13:07:08 -0400

    Change datasim ns to have API functions

--- src/main/com/yetanalytics/datasim.clj
+++ src/main/com/yetanalytics/datasim.clj
@@ -1,17 +1,42 @@
 (ns com.yetanalytics.datasim
   (:require [clojure.spec.alpha :as s]
-            [com.yetanalytics.datasim.input :as input]))
+            [com.yetanalytics.datasim.input :as input]
+            [com.yetanalytics.datasim.sim   :as sim]))
 
-;; Inputs
-;; (location | literal) -> read -> validate ->
+(s/fdef read-input
+  :args (s/cat :location string?)
+  :ret ::input)
 
-;; Comprehensive Spec of all input data for a simulation
-#_(s/def ::input
-  (s/keys :req-un [::input/profiles]))
+(defn read-input
+  "Read input at `location` and return DATASIM input for use with
+   other functions."
+  [location]
+  (input/from-location :input :json location))
 
+;; TODO: Add fdefs for generation functions
+;; TODO: Extract more implementation code from `sim` namespace
 
+(defn generate-seq
+  "Given `input`, produce a lazy sequence of statements in a synchronous
+   fashion."
+  [input & kwargs]
+  (apply sim/sim-seq input kwargs))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn generate-map
+  "Given `input`, produce a map from actor IFIs to lazy sequences of statements
+   that use those actors, all in a synchronous fashion."
+  [input & kwargs]
+  (apply sim/build-skeleton input kwargs))
+
+(defn generate-seq-async
+  "Given `input`, produce a `core.async` channels that contains a generated
+   sequence of simulated statements; this is for parallel generation."
+  [input & kwargs]
+  (apply sim/sim-chan input kwargs))
+
+(defn generate-map-async
+  "Given `input`, produce a map from actor IFIs to `core.async` channels,
+   each with their own generated sequence of simulated statements; this
+   is for parallel generation."
+  [input & kwargs]
+  (apply sim/sim-chans input kwargs))
--- src/test/com/yetanalytics/datasim_test.clj
+++ src/test/com/yetanalytics/datasim_test.clj
@@ -1,10 +1,13 @@
-(ns com.yetanalytics.datasim.sim-test
+(ns com.yetanalytics.datasim-test
   (:require [clojure.test :refer [deftest testing is are]]
             [clojure.core.async :as a]
             [clojure.spec.alpha :as s]
-            [xapi-schema.spec :as xs]
-            [com.yetanalytics.datasim.sim :as sim :refer [build-skeleton sim-seq]]
-            [com.yetanalytics.datasim.test-constants :as const]))
+            [xapi-schema.spec   :as xs]
+            [com.yetanalytics.datasim.test-constants :as const]
+            [com.yetanalytics.datasim :refer [generate-map
+                                              generate-seq
+                                              generate-map-async
+                                              generate-seq-async]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Constants
@@ -69,15 +72,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(deftest build-skeleton-test
+(deftest generate-map-test
   (testing "given valid input, returns a valid skeleton"
     (is (s/valid?
          :com.yetanalytics.datasim.sim/skeleton
-         (build-skeleton const/simple-input)))))
+         (generate-map const/simple-input)))))
 
 (deftest disjoint-rng-test
   (testing "Make sure RNGs aren't shared across threads."
-    (let [skeleton (build-skeleton (assoc-in const/simple-input
+    (let [skeleton (generate-map (assoc-in const/simple-input
                                              [:parameters :end]
                                              nil))]
       (are [actor-id] (let [statement-seq (get skeleton
@@ -91,7 +94,7 @@
 
 (deftest xapi-test
   (testing "sim returns valid xapi statements"
-    (let [skeleton (build-skeleton (assoc-in const/simple-input
+    (let [skeleton (generate-map (assoc-in const/simple-input
                                              [:parameters :end]
                                              nil))]
       (are [actor-id] (s/valid? (s/every ::xs/statement)
@@ -105,20 +108,20 @@
     (is (s/valid? ::xs/statement
                   (-> const/simple-input
                       (assoc-in [:parameters :end] nil)
-                      build-skeleton
+                      generate-map
                       (get bob-mbox)
                       (nth 10000))))))
 
-(deftest sim-seq-test
+(deftest generate-seq-test
   (testing "returns statements"
-    (is (s/valid? (s/every ::xs/statement) (sim-seq const/simple-input))))
+    (is (s/valid? (s/every ::xs/statement) (generate-seq const/simple-input))))
   (testing "respects max param"
-    (let [ret (sim-seq (assoc-in const/simple-input [:parameters :max] 3))]
+    (let [ret (generate-seq (assoc-in const/simple-input [:parameters :max] 3))]
       (is (s/valid? (s/every ::xs/statement) ret))
       (is (= 3 (count ret)))))
   (testing "respects from param"
-    (let [[s0 s1 & _] (sim-seq const/simple-input)
-          [s1' & _]   (sim-seq (assoc-in const/simple-input
+    (let [[s0 s1 & _] (generate-seq const/simple-input)
+          [s1' & _]   (generate-seq (assoc-in const/simple-input
                                          [:parameters :from]
                                          (get-timestamp s0)))]
       (is (not= s0 s1'))
@@ -133,7 +136,7 @@
                    (update :parameters
                            assoc
                            :gen-profiles [cmi5-id])
-                   sim-seq
+                   generate-seq
                    (->> (map get-context-category-activities))
                    distinct))))
       (testing "respects gen-patterns param"
@@ -142,7 +145,7 @@
                    (update :parameters
                            assoc
                            :gen-patterns [tla-completed-session-id])
-                   sim-seq
+                   generate-seq
                    (->> (map get-context-category-activities))
                    distinct))))
       (testing "allows referential use of non-gen profiles"
@@ -152,11 +155,11 @@
                    (update :parameters
                            assoc
                            :gen-patterns [referential-completed-session-id])
-                   sim-seq
+                   generate-seq
                    (->> (map get-context-category-activities))
                    distinct))))))
   (testing "respects agent selection"
-    (let [ret (sim-seq (assoc-in const/simple-input [:parameters :max] 3)
+    (let [ret (generate-seq (assoc-in const/simple-input [:parameters :max] 3)
                        ;; specify we only want the given agent(s)
                        :select-agents [bob-mbox])]
       (is (every?
@@ -164,16 +167,16 @@
            ret))))
   (testing "only actors in personae are generated"
     (is (= #{alice-mailto bob-mailto fred-mailto}
-           (->> const/simple-input sim-seq (map get-actor-mbox) set))))
+           (->> const/simple-input generate-seq (map get-actor-mbox) set))))
   (testing "can apply object override"
-    (let [ret (sim-seq (assoc const/simple-input
+    (let [ret (generate-seq (assoc const/simple-input
                               :alignments const/override-alignments)
                        :select-agents [bob-mbox])]
       (is (every? #(or (= override-activity-1 %)
                        (= override-activity-2 %))
                   (map get-object ret)))))
   (testing "can apply multiple personae"
-    (let [ret (sim-seq (update const/simple-input
+    (let [ret (generate-seq (update const/simple-input
                                :personae-array conj const/tc3-personae))
           ids (map get-actor-mbox ret)]
       (is (= #{;; simple personae
@@ -191,11 +194,11 @@
 
 (deftest sim-chan-test
   (testing "async statement gen produces valid statements"
-    (let [agent-chan (-> const/simple-input sim/sim-chans (get alice-mbox))
+    (let [agent-chan (-> const/simple-input generate-map-async (get alice-mbox))
           async-res  (a/<!! (a/into [] agent-chan))]
       (is (s/valid? (s/every ::xs/statement) async-res))))
   (testing "sync vs async statement gen has same result"
     (let [input     const/simple-input
-          sync-res  (->> input sim/sim-seq)
-          async-res (->> input sim/sim-chan (a/into []) a/<!!)]
+          sync-res  (->> input generate-seq)
+          async-res (->> input generate-seq-async (a/into []) a/<!!)]
       (is (= sync-res async-res)))))
